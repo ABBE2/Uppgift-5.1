b44847030393b649f46324c63f848531fadc79aa
Admin <Admin@TEB127030>
2023-03-31 08:49:57 +0200

    Add project files.

---
+++ Uppgift 5.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace uppgift5._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] meningar = new string[5];
+
+            meningar[0] = "Hej där.";
+            meningar[1] = "Hur mår du idag?";
+            meningar[2] = "Vad är din favorit mat?";
+            meningar[3] = "Vilket fint väder vi har idag.";
+            meningar[4] = "Du tittar på en datorskärm just nu.";
+
+
+            for (int i = 0; i < meningar.Length; i++)
+            {
+                Console.WriteLine(meningar[i]);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
